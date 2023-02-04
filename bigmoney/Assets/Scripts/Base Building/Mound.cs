0298bdc2eb62e250f2ebaac765ca29e0aa4db8a2
Kyle <reisingkyle07@gmail.com>
2023-02-04 14:25:51 -0500

    market, dynamic seed amount

--- bigmoney/Assets/Scripts/Base Building/Mound.cs
+++ bigmoney/Assets/Scripts/Base Building/Mound.cs
@@ -7,7 +7,7 @@ using UnityEngine.EventSystems;
 public class Mound : MonoBehaviour, IDropHandler
 {
     public RectTransform RT;
-
+    public Item i;
 
 
     public void OnDrop(PointerEventData eventData)
@@ -16,9 +16,10 @@ public class Mound : MonoBehaviour, IDropHandler
         //Debug.Log("YOU HAVE BEEN DROPPED ON.");
         //event data contains information about "what" was dragged onto us.
         //eventData.pointerDrag is the literal gameObject that's on top of us.  Let's get an item from the gameobject!
-        Item i = eventData.pointerDrag.GetComponent<Item>();
-
-
+        i = eventData.pointerDrag.GetComponent<Item>();
+        Debug.Log(i.amount);
+        i.amount -= 1;
+        
         
         GameObject G = Instantiate(i.PlaceableObject, RT);
 
